80b220866ee7fa5a7b139dde4e3ac4236e048a41
TinaGrigorova <tina.grigorova@students.codeinstitute.net>
2024-08-08 14:42:46 +0000

    add DOM elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-const questionContainer 
+const questionContainer = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const anwerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+const restartButton = document.getElementById("restart-btn");
+const resultDiv = document.getElementById("result");
