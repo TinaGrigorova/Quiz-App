72ebd1a0d41639666926bebcc5353beea7643c07
TinaGrigorova <tina.grigorova@students.codeinstitute.net>
2024-08-15 21:47:00 +0000

    add ref for footer element

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 const restartButton = document.getElementById("restart-btn");
 const resultDiv = document.getElementById("result");
+const footer = document.getElementById("footer");
 
 let shuffledQuestions, currentQuestionIndex, score;
 
