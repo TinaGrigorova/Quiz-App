5a4cda54cf8c55c23c1dd0dcf2d1e23dfa845ba9
TinaGrigorova <tina.grigorova@students.codeinstitute.net>
2024-08-15 13:44:20 +0000

    remove paragraph and div class container from inner HTML , add style to start page and add some comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -208,6 +208,7 @@ function startQuiz() {
     setNextQuestion();
 }
 
+// Function to get a random subset of questions
 function getRandomQuestions(arr, num) {
     const shuffled = arr.sort(() => 0.5 - Math.random());
     return shuffled.slice(0, num);
